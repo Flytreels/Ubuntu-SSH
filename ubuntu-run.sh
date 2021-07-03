89e29d1f86f50c298d103b2345d30acc69eb45b7
Flytreels <66539732+Flytreels@users.noreply.github.com>
2021-07-03 18:21:01 +0800

    Update ubuntu-run.sh

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -1,4 +1,9 @@
 sudo apt -y purge azure-cli ghc* zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php*
 sudo apt update
-        sudo apt -y autoremove --purge
-        sudo apt clean
+sudo apt -y autoremove --purge
+sudo apt clean
+sudo snap remove lxd
+sudo snap remove core18
+sudo snap remove snapd
+sudo apt install fish git
+sudo rm -rf /mnt/*
